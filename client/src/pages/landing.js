f80eb0a167f12fc0d6734adc82e843a89ad8965f
rakshith <rakshit.s.gowda@gmail.com>
2022-01-06 13:26:00 +0530

    register page styling and wrapper set up

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,25 +1,28 @@
+import Wrapper from '../assets/css-wrapper/landing-wrapper'
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
 function Landing() {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt='job-stall' className='logo' />
       </nav>
       <div className='container page'>
         <div className='info'>
           <h1>
-            Job-<span>Stall</span>
+            <span>Job</span>-Stall
           </h1>
           <p>
             Fastest way to market your needs creativly and finding the cool new
             job or to get the things done with respect to specific skills that
             you need, in the demanding world we are in now ..!!
           </p>
+          <button className='btn btn-hero'>Register / Login</button>
         </div>
+        <img src={main} alt='job-stall-main-pic' className='img main-img' />
       </div>
-    </main>
+    </Wrapper>
   )
 }
 
