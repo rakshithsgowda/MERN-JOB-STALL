db195a606b2e1e6bafded8a657a6b04aa56a8870
rakshith <rakshit.s.gowda@gmail.com>
2022-02-01 10:45:11 +0530

    error handler middleware

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import express from 'express'
+import errorHandlerMiddleware from './middlewares/error-handler.js'
+import NotFoundMiddleware from './middlewares/NotFound.js'
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  throw new Error('Something bad happened.')
+  res.send('welcome')
+})
+
+// ---------------------------------------------------------------------------------------
+// --------------------MIDDLEWARES-----------------------------------------------------
+// ---------------------------------------------------------------------------------------
+app.use(NotFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+// ---------------------------------------------------------------------------------------
+// --------------------LISTENING ON PORT-----------------------------------------------------
+// ---------------------------------------------------------------------------------------
 app.listen(PORT, () => {
   console.log(`listening to the port on ${PORT}`)
 })
