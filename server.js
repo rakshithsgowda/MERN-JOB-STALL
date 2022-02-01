1b5e4247f5a7280c59a5e651340f6a51045d113a
rakshith <rakshit.s.gowda@gmail.com>
2022-02-01 10:51:18 +0530

    dotenv installed backnd

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 import errorHandlerMiddleware from './middlewares/error-handler.js'
 import NotFoundMiddleware from './middlewares/NotFound.js'
 
