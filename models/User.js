6467fd1020a39d2010bd912ace069fa4bb2c84ec
rakshith <rakshit.s.gowda@gmail.com>
2022-02-02 13:50:06 +0530

    user model validation with validator

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 
+import validator from 'validator'
+
 const UserSchema = new mongoose.Schema({
   // name,email,password,lastname,location,
   name: {
@@ -12,6 +14,10 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'provide working email',
+    },
     unique: true,
   },
   password: {
