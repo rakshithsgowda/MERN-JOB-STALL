b77749a7d96e33aeea2f18c6457f9d7a258313fc
rakshith <rakshit.s.gowda@gmail.com>
2022-02-02 18:54:14 +0530

    missing field error handling

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -7,7 +7,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     msg: 'sommething went wrong,try again later',
   }
-  res.status(defaultError.StatusCode).json({ msg: err })
+
+  if (err.name === 'ValidationError') {
+    defaultError.StatusCode = StatusCodes.BAD_REQUEST
+    // defaultError.msg = err.msg
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
+  }
+
+  res.status(defaultError.StatusCode).json({ msg: defaultError.msg })
+
+  // res.status(defaultError.StatusCode).json({ msg: err })
 }
 
 export default errorHandlerMiddleware
